d1fac6b6d5d42f9c0adaa469443e476581d41e1a
Fred Joya <fred.jxx@protonmail.com>
2025-03-01 04:14:31 +0000

    This commit initialises the database by setting up RDS, local PostgreSQL, schema, bash scripts, and seed data

---
+++ backend-flask/db/seed.sql
@@ -0,0 +1,15 @@
+-- this file was manually created
+INSERT INTO public.users (display_name,email,  handle, cognito_user_id)
+VALUES
+  ('Andrew Brown', 'andrew@exampro.co', 'andrewbrown' ,'MOCK'),
+  ('Andrew Bayko', 'bayko@exampro.com', 'bayko' ,'MOCK'),
+  ('Londo Mollari', 'lmollari@centari.com', 'londo' ,'MOCK'),
+  ('Bart Simpson', 'bart@simpson.com', 'bart' ,'MOCK');
+
+INSERT INTO public.activities (user_uuid, message, expires_at)
+VALUES
+  (
+    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    'This was imported as seed data!',
+    current_timestamp + interval '10 day'
+  )
