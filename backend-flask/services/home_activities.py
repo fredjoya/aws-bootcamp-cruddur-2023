5c36b2465eb3601bf06dafe625aa29865cdc7954
Fred Joya <fred.jxx@protonmail.com>
2025-03-01 20:17:47 +0000

    Modified docker-compose.yaml to pass the connection URL as an environment variable.

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
+from lib.db import pool
+
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
@@ -13,6 +15,19 @@ class HomeActivities:
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
+
+      sql = """
+      SELECT * FROM activities
+      """
+      with pool.connection() as conn:
+        with conn.cursor() as cur:
+          cur.execute(sql)
+          # this will return a tuple
+          # the first field being the data
+          json = cur.fetchall()
+      return json[0]
+      return results
+
       
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
