d6d0bfc0a95286b7d410deb36e56b141802d7e4b
Fred Joya <fred.jxx@protonmail.com>
2025-03-02 23:10:12 +0000

    refactor the db library

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -29,6 +29,26 @@ class HomeActivities:
       return results
 
       
+from datetime import datetime, timedelta, timezone
+from opentelemetry import trace
+
+from lib.db import db
+
+#tracer = trace.get_tracer("home.activities")
+
+class HomeActivities:
+  def run(cognito_user_id=None):
+    #logger.info("HomeActivities")
+    #with tracer.start_as_current_span("home-activites-mock-data"):
+    #  span = trace.get_current_span()
+    #  now = datetime.now(timezone.utc).astimezone()
+    #  span.set_attribute("app.now", now.isoformat())
+    sql = db.template('activities','home')
+    results = db.query_array_json(sql)
+    return results
+
+
+      
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Andrew Brown',
